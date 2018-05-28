f7cfb2c06d3361144144c260c9aa12779c9846ca
NathalieFroissart <nathalie.froissart@outlook.com>
2018-05-28 13:38:08 +0200

    hej

--- logreg/startAdmin.php
+++ logreg/startAdmin.php
@@ -1,25 +1,25 @@
 <!DOCTYPE html>
 <html>
- <head> 
- 	<link rel="stylesheet" href="logreg.css">
-  <link href="https://fonts.googleapis.com/css?family=Tajawal" rel="stylesheet">
- 	<meta charset="UTF-8">
- </head>
- <body>
- <title>Schema</title>
- <div class="navbar">
-  <a href="#home">Logga ut</a>
-  <a href="#img"> <img src="../Bilder/header2.png" alt="logpic" id="logo"></a>
-	</div>
-<div class="sidenav">
-  <a href="#about">Start</a>
-  <a href="#clients">Inställningar för Kalender</a>
-  <a href="#contact">Admin konto</a>
-  <a href="#contact">Kundlista</a>
-</div>
+     <head> 
+         	<link rel="stylesheet" href="logreg.css">
+          <link href="https://fonts.googleapis.com/css?family=Tajawal" rel="stylesheet">
+         	<meta charset="UTF-8">
+     </head>
+     <body>
+           <title>Schema</title>
+           <div class="navbar">
+              <a href="../index.html">Logga ut</a>
+              <a> <img src="../Bilder/header2.png" alt="logpic" id="logo"></a>
+           </div>
+           <div class="sidenav">
+              <a href="startAdmin.php">Start</a>
+              <a href="userlist.php">Kund Inställningar</a>
+              <a href="AdminKonto.html">Admin konto</a>
+            </div>
 
+            <p class= "hello"> Hej och välkommen admin! </p>
 
-		<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-		<script src="logreg.js"></script>
- </body>
+          		<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+          		<script src="logreg.js"></script>
+     </body>
 </html>
